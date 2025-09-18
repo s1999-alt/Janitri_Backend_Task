ea2c8354119610f416073b953930a57510e67927
Siyad Savad <siyadsavad313@gmail.com>
2025-09-18 09:08:33 +0530

    views and urls created for register and login with jwt token

--- backend/janitri_backend/users/views.py
+++ backend/janitri_backend/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import RegisterSerializer, UserSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+  queryset = User.objects.all()
+  serializer_class = RegisterSerializer
+  permission_classes = [permissions.AllowAny]
+
+
+
