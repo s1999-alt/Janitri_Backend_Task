c036d9e15b5e0898287c089c540d76abc25f8472
Siyad Savad <siyadsavad313@gmail.com>
2025-09-18 13:24:32 +0530

    permissions changed and setup for every user in userapp

--- backend/janitri_backend/users/permissions.py
+++ backend/janitri_backend/users/permissions.py
@@ -1,7 +1,23 @@
 from rest_framework import permissions
 
-class IsDoctorOrReadOnly(permissions.BasePermission):
+# class IsDoctorOrReadOnly(permissions.BasePermission):
+#   def has_permission(self, request, view):
+#     if request.method in permissions.SAFE_METHODS:
+#       return request.user and request.user.is_authenticated
+#     return request.user and request.user.is_authenticated and (request.user.role in ['doctor','nurse','admin'])
+
+class IsAdmin(permissions.BasePermission):
   def has_permission(self, request, view):
-    if request.method in permissions.SAFE_METHODS:
-      return request.user and request.user.is_authenticated
-    return request.user and request.user.is_authenticated and (request.user.role in ['doctor','nurse','admin'])
+    return request.user.is_authenticated and request.user.role == 'admin'
+  
+class IsDoctor(permissions.BasePermission):
+   def has_permission(self, request, view):
+     return request.user.is_authenticated and request.user.role == 'doctor'
+   
+class IsNurse(permissions.BasePermission):
+  def has_permission(self, request, view):
+    return request.user.is_authenticated and request.user.role == 'nurse'
+
+class IsAdminOrDoctor(permissions. BasePermission):
+  def has_permission(self, request, view):
+    return request.user.is_authenticated and request.user.role in ['admin', 'doctor']
