8f00232f44157c884b59079e382f604d3890b9d0
Siyad Savad <siyadsavad313@gmail.com>
2025-09-18 13:33:20 +0530

    pevious permissions in patients views is changed to new

--- backend/janitri_backend/patients/views.py
+++ backend/janitri_backend/patients/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from .models import Patient
 from .serializers import PatientSerializer
 from rest_framework.permissions import IsAuthenticated
-from users.permissions import IsDoctorOrReadOnly
+from users.permissions import IsAdminOrDoctor
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -11,12 +11,15 @@ from django_filters.rest_framework import DjangoFilterBackend
 class PatientListCreateView(generics.ListCreateAPIView):
   queryset = Patient.objects.all().order_by('-created_at')
   serializer_class = PatientSerializer
-  permission_classes = [IsDoctorOrReadOnly]
   filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
   filterset_fields = ['medical_id','gender']
   search_fields = ['name','medical_id']
   ordering_fields = ['created_at','name']
 
+  def get_permissions(self):
+     if self.request.method == 'POST':
+        return [IsAdminOrDoctor()]
+     return [IsAuthenticated()]
 
   def perform_create(self, serializer):
     serializer.save(created_by=self.request.user)
